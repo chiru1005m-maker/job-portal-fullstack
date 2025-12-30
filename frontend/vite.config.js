bd155dda242708da08c3daf998b47155098e9f2e
Chiranjeevi <chiru1005m@gmail.com>
2025-12-30 11:12:51 +0530

    fix: correct vite proxy and resolve react error 31

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:8080'
+      '/api': {
+        target: 'https://job-portal-api-lghb.onrender.com',
+        changeOrigin: true,
+        secure: false,
+      }
     }
   }
 })
